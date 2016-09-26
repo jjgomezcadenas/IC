0aa520ae26bd00332deccdf807e8b0d604c47688
Vicente Herrero Bosch <viherbos@amic-vaio.tlc.upv.es>
2016-09-26 15:59:45 +0200

    LabTools Cleanup

--- LabTools/find_coeff_II.py
+++ LabTools/find_coeff_II.py
@@ -7,13 +7,17 @@ Created on Thu Jul 28 00:24:52 2016
 
 import numpy as np
 import matplotlib.pyplot as plt
-import DBLR
+from BLR import BLR
 from panel_to_hdf5 import read_panel_hdf5
 from scipy.optimize import curve_fit
 from scipy.optimize import leastsq
 import pandas as pd
+import FEParam as FP
+
+
 
 thr_ener_delta = 50
+
 
 
 def read_panel_hdf5(in_path, PMT, event):
@@ -33,20 +37,17 @@ def find_coeff_II(  LIMIT_L, LIMIT_H, PULSE_R,
 	# LIMIT_L / LIMIT_H : Interesting part of the signal
 	# hdf5_file, Channel, event  : Storage file, Channel and Event
 
+	thr3_a = 0.001*FP.NOISE_ADC
 
-	thr3 = 0.0001 
 	# No fast return to baseline
 	
 	############## AUXILIARY TOOLS #########################################
 	
 	def BLR_pulse(x, coef, f, X_x):
 		# Applies BLR reconstruction function to f signal using coef
-
-		signal_r,energy = DBLR.BLR(signal_daq=(4096-f[X_x].flatten().astype(float)),
-							coef=coef,
-							n_sigma = 4, NOISE_ADC=0.75,
-							thr1 = 4, thr2 = 1, thr3 = thr3, plot = False)
-		return signal_r[x]
+		signal = BLR(signal_daq=(4096-f[X_x].flatten().astype(float)), 
+									coef=coef, mau_len=250,	thr1 = 1.5*FP.NOISE_ADC, thr2 = 0, thr3= thr3_a)
+		return signal.signal_r[x]
 		
 
 	def find_baseline(x):
@@ -84,8 +85,8 @@ def find_coeff_II(  LIMIT_L, LIMIT_H, PULSE_R,
 	ErrorFunc = lambda coef,x: np.std(func(coef,x),ddof=0)
 
 	p0 = 1.4E-3
-	coeff,cov,infodict,mesg,ier = leastsq(ErrorFunc,p0,args=(range(LIMIT_L,LIMIT_H,1)),
-											full_output=True)
+	coeff,cov,infodict,mesg,ier = leastsq(ErrorFunc,p0,
+										args=(range(LIMIT_L,LIMIT_H,1)),full_output=True)
 	ss_err=(infodict['fvec']**2).sum()
 	
 
@@ -99,37 +100,40 @@ def find_coeff_II(  LIMIT_L, LIMIT_H, PULSE_R,
 # beginning of the tail just after the failing edge and the LED transient
 
 
-	out1,energy1 = DBLR.BLR(signal_daq=(4096-signal[X].flatten().astype(float)),
+	out1 = BLR(signal_daq=(4096-signal[X].flatten().astype(float)),
 							coef=coeff,
-							n_sigma = 4, NOISE_ADC=0.75,
-							thr1 = 4, thr2 = 1, thr3 = thr3, plot = False)
+							mau_len=250,
+							thr1 = 1.5*FP.NOISE_ADC, thr2 = 0, 
+							thr3 = thr3_a)
 
-	out2,energy2 = DBLR.BLR(signal_daq=(4096-signal[X].flatten().astype(float)),
-							coef=coeff*1.025,
-							n_sigma = 4, NOISE_ADC=0.75,
-							thr1 = 4, thr2 = 1, thr3 = thr3, plot = False)
+	out2 = BLR(signal_daq=(4096-signal[X].flatten().astype(float)),
+							coef=coeff*1.1,
+							mau_len=250,
+							thr1 = 1.5*FP.NOISE_ADC, thr2 = 0, 
+							thr3 = thr3_a)
 
-	out3,energy3 = DBLR.BLR(signal_daq=(4096-signal[X].flatten().astype(float)),
-							coef=coeff*0.975,
-							n_sigma = 4, NOISE_ADC=0.75,
-							thr1 = 4, thr2 = 1, thr3 = thr3, plot = False)
+	out3 = BLR(signal_daq=(4096-signal[X].flatten().astype(float)),
+							coef=coeff*0.9,
+							mau_len=250,
+							thr1 = 1.5*FP.NOISE_ADC, thr2 = 0, 
+							thr3 = thr3_a)
 
 	# This is to keep an eye on the evolution of the error (+-2.5% change in coeff)
 
 	plt.figure()
 	plt.plot(X, 4096-signal)			
-	plt.plot(X, out1, 'r--', linewidth=1)
-	plt.plot(X, out2, 'b--', linewidth=2)
-	plt.plot(X, out3, 'b--', linewidth=2)
+	plt.plot(X, out1.signal_r, 'r--', linewidth=1)
+	plt.plot(X, out2.signal_r, 'b--', linewidth=2)
+	plt.plot(X, out3.signal_r, 'b--', linewidth=2)
 	plt.show()
 
-	thr_ener = baseline+thr_ener_delta
+	thr_ener = thr_ener_delta
 
-	ener1=(out1>thr_ener)*out1
-	ener2=(out2>thr_ener)*out2
-	ener3=(out3>thr_ener)*out3
+	ener1=(out1.signal_r>thr_ener)*out1.signal_r
+	ener2=(out2.signal_r>thr_ener)*out2.signal_r
+	ener3=(out3.signal_r>thr_ener)*out3.signal_r
 
-	print (100*(ener2.sum()-ener1.sum())/ener1.sum(), ener1.sum(), 100*(ener2.sum()-ener1.sum())/ener1.sum())
+	print (100*(ener2.sum()-ener1.sum())/ener1.sum(), ener1.sum(), 100*(ener3.sum()-ener1.sum())/ener1.sum())
 
 	# This is to get an idea of the coeff error effect on energy calculations (10% error on coeef --> 1% energy error)
 	# This coeff error is absolutely linear so it is calibrable away
@@ -190,18 +194,18 @@ def main():
 	# 	print (coeff)
 	   	
 
-	# for i in range(2,24,1): 
-	#    	LIMIT_L       = 3850
-	#    	LIMIT_H       = 4250
-	#    	PULSE_R  	 = 1736
-	# 	PULSE_L 	 = 2000
-	#    	hdf5_file 	 = 'F:\DATOS_DAC\CALIBRATION\cal_50u.h5.z'
-	#    	Channel 		     = i
-	#    	event_range      = range(0,500,1)
+	for i in range(23,24,1): 
+	   	LIMIT_L       = 3850
+	   	LIMIT_H       = 4250
+	   	PULSE_R  	 = 1736
+		PULSE_L 	 = 2000
+	   	hdf5_file 	 = '/mnt/WINDOWS_ntfs/DATOS_DAC/CALIBRATION/cal_50u.h5.z'
+	   	Channel 		     = i
+	   	event_range      = range(0,500,1)
 
-	# 	coeff[i,3] = find_coeff_II( LIMIT_L, LIMIT_H, PULSE_R, PULSE_L,
-	# 								hdf5_file, Channel, event_range )	
-	# 	print (coeff)
+		coeff[i,3] = find_coeff_II( LIMIT_L, LIMIT_H, PULSE_R,
+									hdf5_file, Channel, event_range )	
+		print (coeff)
 
 
 	# for i in range(2,24,1): 
