12515e47c5e8a7b11fd916c188c612aed86f3c88
jjgomezcadenas <jjgomezcadenas@gmail.com>
2016-10-21 11:12:05 +0200

    further commits

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 export PFMW=/System/Library/Frameworks/Python.framework/Versions/Current/
 export ICDIR=/Users/jjgomezcadenas/Documents/Development/NEXT/IC
-export CyDIR=/Users/jjgomezcadenas/Documents/Development/NEXT/CyIC
 export PYTHONPATH=$ICDIR:$ICDIR/Cities:$ICDIR/Cython:$ICDIR/Sierpe:$ICDIR/Core
 export PYTHONPATH=$PYTHONPATH:$ROOTSYS/lib:/usr/local/lib:/$PFMW
 export PS1="IC->"
