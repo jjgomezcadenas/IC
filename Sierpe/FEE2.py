8a94a7a3f7f9a8ba2ccd36aba903900593c4034d
Gonzalo <gonzaponte@gmail.com>
2016-10-23 02:27:21 +0200

    pep8+pyflakes

--- Sierpe/FEE2.py
+++ Sierpe/FEE2.py
@@ -1,269 +1,270 @@
+"""
+"""
+
 from __future__ import print_function
-from Util import *
+
+import math
+import numpy as np
+from scipy import signal as SGN
+import matplotlib.pyplot as plt
+
+import system_of_units as units
 import mplFunctions as mpl
 import FEParam as FP
 import SPE as SP
-from scipy import signal as SGN
-import numpy as np
-#from deModel import arrayFixedInt
-#from deModel import DeFixedInt
 
 
 def DownScaleSignal(signal_t, signal, scale):
- 	"""
- 	downscales the time and signal vectors
- 	"""
-	signal_d=SGN.decimate(signal,scale,ftype='fir')
-	signal_t_d = SGN.decimate(signal_t,scale,ftype='fir')
+    """
+    Downscales the time and signal vectors
+    """
+    signal_d = SGN.decimate(signal, scale, ftype="fir")
+    signal_t_d = SGN.decimate(signal_t, scale, ftype="fir")
 
-	#pulse_plot(signal_t_d, signal_d)
-	return signal_t_d, signal_d
+    # pulse_plot(signal_t_d, signal_d)
+    return signal_t_d, signal_d
+
 
 def down_scale_signal_(signal, scale):
- 	"""
-    downscales the signal vector. Re-scale the energy
- 	"""
-	signal_d=SGN.decimate(signal,scale,ftype='fir')
+    """
+    Downscales the signal vector. Re-scale the energy
+    """
+    signal_d = SGN.decimate(signal, scale, ftype="fir")
 
-	#return signal_d*scale
-	return signal_d
+    return signal_d
 
-def DeconvSimple(signal,signal_inv):
-	"""
-	Deconvolution of the fine-grained fee signal (no DAQ)
-	no noise
-	using true start and end of signals
-	"""
 
-	coef = signal_inv[100]
-	acum = np.zeros(len(signal))
+def DeconvSimple(signal, signal_inv):
+    """
+    Deconvolution of the fine-grained fee signal (no DAQ)
+    no noise
+    using true start and end of signals
+    """
 
-	acum[0]=coef*signal[0]
-	for n in xrange(1,len(signal)):
-		acum[n] = acum[n-1] + signal[n]
+    coef = signal_inv[100]
+    acum = np.zeros(len(signal))
 
-	signal_r = signal + coef*acum
+    acum[0] = coef * signal[0]
+    for n in xrange(1, len(signal)):
+        acum[n] = acum[n-1] + signal[n]
 
-	return signal_r
+    signal_r = signal + coef * acum
+
+    return signal_r
 
 
 ##########################################################
 class Filter:
 
- 	def __init__(self,ftype='high',fc=5E3,fs= 1e+9):
-		"""
-		Defines a Butterworth HPF (high pass frequency) or LPF (low pass frequencey) filter
-		the default sampling frequencey is 1 GHz (inverse of 1 ns time)
-		type may be equal to hig or low
-		"""
-		self.fc = fc
-		self.fs = fs
-		self.W = 2*self.fc/self.fs
-		self.ftype = ftype
-		self.b, self.a = SGN.butter(1, self.W, btype=self.ftype, analog=False, output='ba')
-		self.ba, self.aa = SGN.butter(1, 2*self.fc, btype=self.ftype, analog=True, output='ba')
+    def __init__(self, ftype="high", fc=5E3, fs=1e+9):
+        """
+        Defines a Butterworth HPF (high pass frequency) or LPF
+        (low pass frequencey) filter the default sampling frequencey is 1 GHz
+        (inverse of 1 ns time) type may be equal to high or low
+        """
+        self.fc = fc
+        self.fs = fs
+        self.W = 2*self.fc/self.fs
+        self.ftype = ftype
+        self.b, self.a = SGN.butter(1, self.W, btype=self.ftype,
+                                    analog=False, output="ba")
+        self.ba, self.aa = SGN.butter(1, 2*self.fc, btype=self.ftype,
+                                      analog=True, output="ba")
 
-	def FilterCoef(self):
-		"""
-		Returns the filter coefficients
-		"""
-  		return self.b,self.a
+    def FilterCoef(self):
+        """
+        Returns the filter coefficients
+        """
+        return self.b, self.a
 
-  	def FilterAnalogCoef(self):
-		"""
-		Returns the filter coefficients
-		"""
-  		return self.ba,self.aa
+    def FilterAnalogCoef(self):
+        """
+        Returns the filter coefficients
+        """
+        return self.ba, self.aa
 
-	def FilterPulse(self,pulse):
-		"""
-		Filters a pulse
-		"""
-  		return SGN.lfilter(self.b,self.a, pulse)
+    def FilterPulse(self, pulse):
+        """
+        Filters a pulse
+        """
+        return SGN.lfilter(self.b, self.a, pulse)
 
-  	def FilterResponse(self):
-  		"""
-  		Gives the response of the filter y frequency-amplitude
-  		"""
+    def FilterResponse(self):
+        """
+        Gives the response of the filter y frequency-amplitude
+        """
+        self.w, self.h = SGN.freqs(self.ba, self.aa)
+        return self.w, self.h
 
-  		self.w, self.h = SGN.freqs(self.ba, self.aa)
-  		return self.w, self.h
-
-
-	def __str__(self):
-
-		s= """
-		Filter:
-		fc = %7.2f Hz, fs = %7.2f, W = %7.2f Hz type = %s
-		"""%(self.fc, self.fs, self.W, self.ftype)
-		return s
-
+    def __str__(self):
+        s = """
+        Filter:
+        fc = %7.2f Hz, fs = %7.2f, W = %7.2f Hz type = %s
+        """.format(self.fc, self.fs, self.W, self.ftype)
+        return s
 
 
 ###########################################################
-
 class FEE:
-	"""
-	Emulates the effect of the PMT + FEE:
-	1) PMT gain G
-	2) A HPF, due to the FEE decoupling capacitor C and the associated series resitor R
-	3) A LPF that shapes the signal, with a frequencey f and a noise
-	4) A resitor gain RG to transform current in voltage
-	"""
+    """
+    Emulates the effect of the PMT + FEE:
+    1) PMT gain G
+    2) A HPF, due to the FEE decoupling capacitor C and the associated
+       series resitor R
+    3) A LPF that shapes the signal, with a frequencey f and a noise
+    4) A resitor gain RG to transform current in voltage
+    """
 
- 	def __init__(self,PMTG=4.5e6, C=6.75*nF,R= 2350*ohm, f=2E6*hertz, RG=250*ohm):
+    def __init__(self, PMTG=4.5e6, C=6.75*units.nF, R=2350*units.ohm,
+                 f=2E6*units.hertz, RG=250*units.ohm):
+        self.PMTG = PMTG
+        self.C = C
+        self.R = R
+        self.f_LPF = f
+        self.RG = RG
+        self.f_HPF = 1./(2*math.pi*R*C)
+        self.hpf = Filter(ftype="high", fc=self.f_HPF, fs=FP.f_sample)
+        self.lpf = Filter(ftype="low", fc=self.f_LPF, fs=FP.f_sample)
+        self.hpfr = Filter(ftype="high", fc=self.f_HPF, fs=FP.f_sample_DAQ)
 
- 		self.PMTG = PMTG
- 		self.C = C
- 		self.R = R
- 		self.f_LPF = f
- 		self.RG = RG
- 		self.f_HPF=(1./(2*pi*R*C))
- 		self.hpf = Filter(ftype='high',fc=self.f_HPF,fs=FP.f_sample)
-		self.lpf = Filter(ftype='low',fc=self.f_LPF,fs=FP.f_sample)
-		self.hpfr = Filter(ftype='high',fc=self.f_HPF,fs=FP.f_sample_DAQ)
+    def Filter(self, signal):
+        """
+        for an input signal in pes, returns the effect of the electronics
+        """
+        signal_hp = self.hpf.FilterPulse(signal)
+        signal_lp = self.lpf.FilterPulse(signal)
+        signal_hp_lp = self.lpf.FilterPulse(signal_hp)
+        return signal_lp, signal_hp, signal_hp_lp
+
+    def FilterInverse(self, signal):
+        """
+        Returns the inverse of the HPF filter in bins of 1ns
+        """
+        b_HPr, a_HPr = self.hpf.FilterCoef()
+        h_t_inv = SGN.lfilter(a_HPr, b_HPr, signal)
+        return h_t_inv
+
+    def FilterInverseDAQ(self, signal):
+        """
+        Returns the inverse of the HPF filter in bins of time_DAQ (25 ns)
+        """
+        b_HPr, a_HPr = self.hpfr.FilterCoef()
+        h_t_inv = SGN.lfilter(a_HPr, b_HPr, signal)
+        return h_t_inv
+
+    def FEESignal(self, signal_current, noise_rms=0.3*units.mV):
+        """
+        Filters the input signal according to the filters and
+        transforms it in volts
+        """
+        signal_lp, signal_hp, signal_hp_lp = self.Filter(signal_current)
+        noise = self.FEENoise(len(signal_current), noise_rms)
+        return signal_hp_lp*self.RG + noise, signal_lp * self.RG + noise
+
+    def DAQSignal(self, signal_t, signal_fee, noise_rms=0.3*units.mV):
+        """
+        Downscale the signal after the FEE
+        """
+        signal_t_d, signal_d = DownScaleSignal(signal_t, signal_fee,
+                                               int(FP.time_DAQ))
+        signal_daq = signal_d/FP.voltsToAdc
+        noise = self.FEENoise(len(signal_daq), noise_rms/FP.voltsToAdc)
+        return signal_t_d, signal_daq + noise
+
+    def daqSignal(self, signal_fee, noise_rms=FP.NOISE_FEE_rms):
+        """
+        downscale the signal after the FEE
+        """
+        signal_d = down_scale_signal_(signal_fee, int(FP.time_DAQ))
+        signal_daq = signal_d/FP.voltsToAdc
+        noise = self.FEENoise(len(signal_daq), noise_rms/FP.voltsToAdc)
+        return signal_daq + noise
+
+    def FEENoise(self, signal_length, noise_rms=FP.NOISE_FEE_rms):
+        """
+        filters the input signal according to the filters and
+        transforms it in volts
+        """
+        if noise_rms > 0:
+            noise = np.random.normal(0, noise_rms, signal_length)
+            return noise
+        else:
+            return np.zeros(signal_length)
+
+    def VSignal(self, signal_current):
+        """
+        Takes a current input signal and transforms it to volts
+        """
+        return signal_current * self.RG
+
+    def InverseSignal(self, signal_t):
+        """
+        Computes the inverse signal for deconvolution
+        """
+        pulse = np.zeros(len(signal_t))
+        pulse[0] = 1
+
+        signal_fee_inv = self.FilterInverse(pulse)
+        return signal_fee_inv
+
+    def InverseSignalDAQ(self, signal_t):
+        """
+        Computes the inverse signal in bins of DAQ
+        """
+        pulse = np.zeros(len(signal_t))
+        pulse[0] = 1
+
+        signal_daq_inv = self.FilterInverseDAQ(pulse)
+        return signal_daq_inv
+
+    def __str__(self):
+        s = """
+        NEW FEE
+        PMT gain = %7.2g
+        decoupling capacitor = %7.2f nF
+        decoupling resistor = %7.2f ohm
+        resitor gain = %7.2f ohm
+        HPF frequency = %7.2g Hz  W_HPF = %7.2g
+        LPF frequency = %7.2g Hz  W_LPF = %7.2g
+        """.format(self.PMTG, self.C/units.nF, self.R/units.ohm,
+                   self.RG/units.ohm, self.f_HPF/units.hertz,
+                   self.hpf.W, self.f_LPF/units.hertz, self.lpf.W)
+        return s
 
 
-  	def Filter(self,signal):
-  	  	"""
-  	  	for an input signal in pes, returns the effect of the electronics
-  	  	"""
-  	  	signal_hp = self.hpf.FilterPulse(signal)
-  	  	signal_lp = self.lpf.FilterPulse(signal)
-  	  	signal_hp_lp = self.lpf.FilterPulse(signal_hp)
-  	  	return signal_lp, signal_hp, signal_hp_lp
-
-  	def FilterInverse(self, signal):
-  		"""
-  		Returns the inverse of the HPF filter in bins of 1ns
-  		"""
-  		b_HPr,a_HPr = self.hpf.FilterCoef()
-		h_t_inv = SGN.lfilter(a_HPr,b_HPr, signal)
-		return h_t_inv
-
-	def FilterInverseDAQ(self, signal):
-		"""
-		Returns the inverse of the HPF filter in bins of time_DAQ (25 ns)
-		"""
-  		b_HPr,a_HPr = self.hpfr.FilterCoef()
-		h_t_inv = SGN.lfilter(a_HPr,b_HPr, signal)
-		return h_t_inv
-
-	def FEESignal(self,signal_current, noise_rms=0.3*mV):
- 		"""
- 		filters the input signal according to the filters and transforms it in volts
- 		"""
- 		signal_lp, signal_hp, signal_hp_lp = self.Filter(signal_current)
- 		noise = self.FEENoise(len(signal_current), noise_rms)
- 		return signal_hp_lp*self.RG + noise, signal_lp * self.RG + noise
-
- 	def DAQSignal(self,signal_t, signal_fee, noise_rms=0.3*mV):
- 		"""
- 		downscale the signal after the FEE
-
- 		"""
-
-		signal_t_d, signal_d = DownScaleSignal(signal_t, signal_fee, int(FP.time_DAQ))
-		signal_daq = signal_d/FP.voltsToAdc
-		noise = self.FEENoise(len(signal_daq), noise_rms/FP.voltsToAdc)
- 		return signal_t_d, signal_daq + noise
-
- 	def daqSignal(self,signal_fee, noise_rms=FP.NOISE_FEE_rms):
- 		"""
- 		downscale the signal after the FEE
-
- 		"""
-
-		signal_d = down_scale_signal_(signal_fee, int(FP.time_DAQ))
-		signal_daq = signal_d/FP.voltsToAdc
-		noise = self.FEENoise(len(signal_daq), noise_rms/FP.voltsToAdc)
- 		return signal_daq + noise
-
- 	def FEENoise(self, signal_length, noise_rms=FP.NOISE_FEE_rms):
- 		"""
- 		filters the input signal according to the filters and transforms it in volts
- 		"""
- 		if noise_rms > 0:
- 			noise = np.random.normal(0, noise_rms, signal_length)
- 			return noise
- 		else:
- 			return np.zeros(signal_length)
-
-
- 	def VSignal(self,signal_current):
- 		"""
- 		Takes a current input signal and transforms it to volts
- 		"""
- 		return signal_current*self.RG
-
- 	def InverseSignal(self, signal_t):
- 		"""
- 		Computes the inverse signal for deconvolution
- 		"""
-		pulse = np.zeros(len(signal_t))
-		pulse[0]=1
-
-		signal_fee_inv = self.FilterInverse(pulse)
-		return signal_fee_inv
-
-	def InverseSignalDAQ(self, signal_t):
-		"""
-		Computes the inverse signal in bins of DAQ
-		"""
-		pulse = np.zeros(len(signal_t))
-		pulse[0]=1
-
-		signal_daq_inv = self.FilterInverseDAQ(pulse)
-		return signal_daq_inv
-
- 	def __str__(self):
-
-		s= """
-		NEW FEE
-  		PMT gain = %7.2g
-  		decoupling capacitor = %7.2f nF
-  		decoupling resistor = %7.2f ohm
-  		resitor gain = %7.2f ohm
-  		HPF frequency = %7.2g Hz  W_HPF = %7.2g
-  		LPF frequency = %7.2g Hz  W_LPF = %7.2g
-  """%(self.PMTG,self.C/nF,self.R/ohm, self.RG/ohm,
-  		self.f_HPF/hertz,self.hpf.W,
-  		self.f_LPF/hertz, self.lpf.W)
-
-		return s
-
-def single_pe(start_pulse=100*ns, end_pulse=500*ns, noise=FP.NOISE_FEE_rms):
+def single_pe(start_pulse=100*units.ns, end_pulse=500*units.ns,
+              noise=FP.NOISE_FEE_rms):
     spe = SP.SPE()
     fee = FEE()
     # PMT response to a single photon (single pe current pulse)
-    signal_t, signal_PE =spe.SpePulse(start_pulse, tmax=end_pulse)
+    signal_t, signal_PE = spe.SpePulse(start_pulse, tmax=end_pulse)
     # spe in voltage (without FEE)
     signal_PE_v = fee.VSignal(signal_PE)
-    #effect of FEE
+    # effect of FEE
     signal_fee = fee.FEESignal(signal_PE, noise_rms=noise)
-    #effect of DAQ
+    # effect of DAQ
     signal_t_daq, signal_daq = fee.DAQSignal(signal_t, signal_fee, noise_rms=0)
-    signal_daq *=FP.time_DAQ #re-scale by decimation factor
-    plt.figure(figsize=(12,12))
+    signal_daq *= FP.time_DAQ  # re-scale by decimation factor
+    plt.figure(figsize=(12, 12))
 
-    ax1 = plt.subplot(2,2,1)
+    ax1 = plt.subplot(2, 2, 1)
     ax1.set_xlim([0, len(signal_t)])
-    mpl.SetPlotLabels(xlabel='t (ns)', ylabel='spe (muA)')
-    plt.plot(signal_t, signal_PE/muA)
+    mpl.SetPlotLabels(xlabel="t (ns)", ylabel="spe (muA)")
+    plt.plot(signal_t, signal_PE/units.muA)
 
-    ax2 = plt.subplot(2,2,2)
+    ax2 = plt.subplot(2, 2, 2)
     ax2.set_xlim([0, len(signal_t)])
-    mpl.SetPlotLabels(xlabel='t (ns)', ylabel='spe (mV)')
-    plt.plot(signal_t, signal_PE_v/mV)
+    mpl.SetPlotLabels(xlabel="t (ns)", ylabel="spe (mV)")
+    plt.plot(signal_t, signal_PE_v/units.mV)
 
-    ax3 = plt.subplot(2,2,3)
+    ax3 = plt.subplot(2, 2, 3)
     ax3.set_xlim([0, len(signal_t)])
-    mpl.SetPlotLabels(xlabel='t (ns)', ylabel='spe+FEE (mV)')
-    plt.plot(signal_t, signal_fee/mV)
+    mpl.SetPlotLabels(xlabel="t (ns)", ylabel="spe+FEE (mV)")
+    plt.plot(signal_t, signal_fee/units.mV)
 
-    ax4 = plt.subplot(2,2,4)
+    ax4 = plt.subplot(2, 2, 4)
     ax4.set_xlim([0, len(signal_t)])
-    mpl.SetPlotLabels(xlabel='t (ns)', ylabel='spe+FEE+DAQ (adc)')
+    mpl.SetPlotLabels(xlabel="t (ns)", ylabel="spe+FEE+DAQ (adc)")
     plt.plot(signal_t_daq, signal_daq)
 
     area = np.sum(signal_daq)
@@ -271,39 +272,42 @@ def single_pe(start_pulse=100*ns, end_pulse=500*ns, noise=FP.NOISE_FEE_rms):
 
     plt.show()
 
-def pulse_train(signal_start=2000*ns, signal_length=5000*ns, daq_window = 20*microsecond, noise=FP.NOISE_FEE_rms):
+
+def pulse_train(signal_start=2000*units.ns, signal_length=5000*units.ns,
+                daq_window=20*units.microsecond, noise=FP.NOISE_FEE_rms):
     signal_end = signal_start + signal_length
     spe = SP.SPE()
     fee = FEE()
     # PMT response to a photon train
-    signal_t, signal_PE = spe.SpePulseTrain(signal_start,signal_end,daq_window)
+    signal_t, signal_PE = spe.SpePulseTrain(signal_start, signal_end,
+                                            daq_window)
     # spe in voltage (without FEE)
     signal_PE_v = fee.VSignal(signal_PE)
-    #effect of FEE
+    # effect of FEE
     signal_fee = fee.FEESignal(signal_PE, noise_rms=noise)
-    #effect of DAQ
+    # effect of DAQ
     signal_t_daq, signal_daq = fee.DAQSignal(signal_t, signal_fee, noise_rms=0)
-    signal_daq *=FP.time_DAQ #re-scale by decimation factor
-    plt.figure(figsize=(12,12))
+    signal_daq *= FP.time_DAQ  # re-scale by decimation factor
+    plt.figure(figsize=(12, 12))
 
-    ax1 = plt.subplot(2,2,1)
+    ax1 = plt.subplot(2, 2, 1)
     ax1.set_xlim([0, len(signal_t)])
-    mpl.SetPlotLabels(xlabel='t (ns)', ylabel='spe train (muA)')
-    plt.plot(signal_t, signal_PE/muA)
+    mpl.SetPlotLabels(xlabel="t (ns)", ylabel="spe train (muA)")
+    plt.plot(signal_t, signal_PE/units.muA)
 
-    ax2 = plt.subplot(2,2,2)
+    ax2 = plt.subplot(2, 2, 2)
     ax2.set_xlim([0, len(signal_t)])
-    mpl.SetPlotLabels(xlabel='t (ns)', ylabel='spe train (mV)')
-    plt.plot(signal_t, signal_PE_v/mV)
+    mpl.SetPlotLabels(xlabel="t (ns)", ylabel="spe train (mV)")
+    plt.plot(signal_t, signal_PE_v/units.mV)
 
-    ax3 = plt.subplot(2,2,3)
+    ax3 = plt.subplot(2, 2, 3)
     ax3.set_xlim([0, len(signal_t)])
-    mpl.SetPlotLabels(xlabel='t (ns)', ylabel='spe+FEE (mV)')
-    plt.plot(signal_t, signal_fee/mV)
+    mpl.SetPlotLabels(xlabel="t (ns)", ylabel="spe+FEE (mV)")
+    plt.plot(signal_t, signal_fee/units.mV)
 
-    ax4 = plt.subplot(2,2,4)
+    ax4 = plt.subplot(2, 2, 4)
     ax4.set_xlim([0, len(signal_t)])
-    mpl.SetPlotLabels(xlabel='t (ns)', ylabel='spe+FEE+DAQ (adc)')
+    mpl.SetPlotLabels(xlabel="t (ns)", ylabel="spe+FEE+DAQ (adc)")
     plt.plot(signal_t_daq, signal_daq)
 
     area = np.sum(signal_daq)
@@ -314,7 +318,7 @@ def pulse_train(signal_start=2000*ns, signal_length=5000*ns, daq_window = 20*mic
 
 
 #
-# if __name__ == '__main__':
+# if __name__ == "__main__":
 # 	fee = FEE()
 # 	print fee
 # 	single_pe(start_pulse=100*ns, end_pulse=1000*ns,noise=0)
@@ -322,11 +326,11 @@ def pulse_train(signal_start=2000*ns, signal_length=5000*ns, daq_window = 20*mic
 # 	signal_t, signal_fee = pulse_train()
 # 	signal_fee_inv = fee.InverseSignal(signal_t)
 # 	mpl.plot_signal(signal_t/ns,signal_fee_inv,
-#                 title = 'Inverse FEE',
+#                 title = "Inverse FEE",
 #                 signal_start=0*ns, signal_end=10*ns,
-#                 units='')
+#                 units="")
 # 	signal_r = DeconvSimple(signal_fee,signal_fee_inv)
 # 	mpl.plot_signal(signal_t/ns,signal_r/mV,
-#                 	title = 'Deconv simple',
+#                 	title = "Deconv simple",
 #                 	signal_start=0, signal_end=len(signal_t),
-#                 	units='mV')
+#                 	units="mV")
--- Sierpe/SPE.py
+++ Sierpe/SPE.py
@@ -1,11 +1,14 @@
-import FEParam as FP
-from Util import *
-from scipy import signal as SGN
+import sys
 import numpy as np
+from scipy import signal as SGN
 
-# spe is parameterized as an isosceles trapezoid of diagonal 
-#   xxxxx-xxxxx      
-#           --- 
+import FEParam as FP
+import system_of_units as units
+
+
+# spe is parameterized as an isosceles trapezoid of diagonal
+#   xxxxx-xxxxx
+#           ---
 #         -     -
 #       -         -
 #     -             -
@@ -13,117 +16,113 @@ import numpy as np
 #   1 2 3 4 515 4 3 2 1
 # x_slope = 5 ns, x_flat = 1 ns
 
+
 ###########################################################
 class SPE:
+    def __init__(self, pmt_gain=4.5e6, x_slope=5*units.ns, x_flat=1*units.ns):
+        """
+        Defines a spe
+        """
+        self.pmt_gain = pmt_gain
+        self.x_slope = x_slope
+        self.x_flat = x_flat
 
-  def __init__(self,pmt_gain=4.5e6,x_slope = 5*ns,x_flat = 1*ns):
-    """
-    Defines a spe 
-    """
-    self.pmt_gain=pmt_gain
-    self.x_slope=x_slope
-    self.x_flat=x_flat
-    
-    self.spe_base = self.x_slope + self.x_flat
-    self.spe_length = 2*self.x_slope + self.x_flat
+        self.spe_base = self.x_slope + self.x_flat
+        self.spe_length = 2*self.x_slope + self.x_flat
 
-    self.A=self.pmt_gain*eplus/self.spe_base  #current  
-    self.V = self.A*FP.spe_i_to_v()
-    self.nADC = int(self.A*FP.spe_i_to_adc())
+        self.A = self.pmt_gain*units.eplus/self.spe_base  # current
+        self.V = self.A*FP.spe_i_to_v()
+        self.nADC = int(self.A*FP.spe_i_to_adc())
 
-    self.t = np.arange(0,self.spe_length,FP.time_step)
-    nns = int(self.x_slope/FP.time_step)
-    nnf = int(self.x_flat/FP.time_step)
-    rise = np.linspace(0,self.A, num=nns)
-    fall = np.linspace(self.A,0, num=nns)
-    flat = self.A*np.ones(nnf)
-    self.spe=np.concatenate((rise,flat,fall))
-    
-        
-  def Spe(self):
-    """
-    Returns a SPE
-    """
-    return self.t,self.spe
+        self.t = np.arange(0, self.spe_length, FP.time_step)
+        nns = int(self.x_slope/FP.time_step)
+        nnf = int(self.x_flat/FP.time_step)
+        rise = np.linspace(0, self.A, num=nns)
+        fall = np.linspace(self.A, 0, num=nns)
+        flat = self.A*np.ones(nnf)
+        self.spe = np.concatenate((rise, flat, fall))
 
-  def SpePulse(self,t0, tmax=1e+6*ns):
-    """
-    Returns a SPE pulse at time t0
-    with baseline extending in steps of time_step from 0 to tmax determined by DELTA_L
-    """
-    n = int(t0/FP.time_step)
-    nmax = int(tmax/FP.time_step)
-    if n >= nmax:
-      print "error in SpePulse n = %d nmax = %d "%(
-        n, nmax)
-      sys.exit()
+    def Spe(self):
+        """
+        Returns a SPE
+        """
+        return self.t, self.spe
 
-    
-    DELTA=np.zeros(nmax)   #Dirac delta of size DELTA_L
-    DELTA[n]=1
-    step = FP.time_step/ns
-    spe_pulse_t =np.arange(0,len(DELTA) + len(self.spe) -1,step)
-    spe_pulse = SGN.convolve(DELTA, self.spe)
-    
-    return spe_pulse_t,spe_pulse
+    def SpePulse(self, t0, tmax=1e+6*units.ns):
+        """
+        Returns a SPE pulse at time t0
+        with baseline extending in steps of time_step from 0 to tmax
+        determined by DELTA_L
+        """
+        n = int(t0/FP.time_step)
+        nmax = int(tmax/FP.time_step)
+        if n >= nmax:
+            print "error in SpePulse n = %d nmax = %d " % (n, nmax)
+            sys.exit()
 
-  def SpePulseTrain(self,tlow,tup,tmax=1e+6*ns):
-    """
-    Returns a train of SPE pulses between tlow and tup separated by tstep
-    """
-    nmin = int(tlow/FP.time_step)
-    nmax = int(tup/FP.time_step)
-    NMAX = int(tmax/FP.time_step)
-    step = FP.time_step/ns
+        DELTA = np.zeros(nmax)  # Dirac delta of size DELTA_L
+        DELTA[n] = 1
+        step = FP.time_step/units.ns
+        spe_pulse_t = np.arange(0, len(DELTA) + len(self.spe) - 1, step)
+        spe_pulse = SGN.convolve(DELTA, self.spe)
+        return spe_pulse_t, spe_pulse
 
-    if nmax >= NMAX:
-      print "error in SpePulse train nmax = %d NMAX = %d "%(
-        nmax, NMAX)
-      sys.exit()
+    def SpePulseTrain(self, tlow, tup, tmax=1e6*units.ns):
+        """
+        Returns a train of SPE pulses between tlow and tup separated by tstep
+        """
+        nmin = int(tlow/FP.time_step)
+        nmax = int(tup/FP.time_step)
+        NMAX = int(tmax/FP.time_step)
+        step = FP.time_step/units.ns
 
-    DELTA=np.zeros(NMAX)
-    DELTA[nmin:nmax+1] = 1
-    spe_pulse_t =np.arange(0,len(DELTA) + len(self.spe) -1,step)
-    spe_pulse = SGN.convolve(DELTA, self.spe)
-      
-    return spe_pulse_t,spe_pulse
+        if nmax >= NMAX:
+            print "error in SpePulse train nmax = %d NMAX = %d" % (nmax, NMAX)
+            sys.exit()
 
-  def SpePulseFromVectorPE(self,cnt):
-    """
-    Returns a train of SPE pulses corresponding to vector cnt
-    """
-    
-    spe_pulse = SGN.convolve(cnt[0:-len(self.spe)+1], self.spe)
-      
-    return spe_pulse
+        DELTA = np.zeros(NMAX)
+        DELTA[nmin:nmax+1] = 1
+        spe_pulse_t = np.arange(0, len(DELTA) + len(self.spe) - 1, step)
+        spe_pulse = SGN.convolve(DELTA, self.spe)
 
-  def __str__(self):
-        
-    s= """
+        return spe_pulse_t, spe_pulse
+
+    def SpePulseFromVectorPE(self, cnt):
+        """
+        Returns a train of SPE pulses corresponding to vector cnt
+        """
+
+        spe_pulse = SGN.convolve(cnt[0:-len(self.spe)+1], self.spe)
+        return spe_pulse
+
+    def __str__(self):
+        s = """
         SPE:
         Gain = %7.2f I (muA) = %7.2f, V (mV) = %7.2f, ADC = %d
-      """%(self.pmt_gain, self.A/microampere, self.V/mV, self.nADC )
-    return s
+        """ % (self.pmt_gain, self.A/units.muA, self.V/units.mV, self.nADC)
+        return s
 
 
 if __name__ == '__main__':
-  from PlotUtil import *
+    import PlotUtil as putil
+    import matplotlib.pyplot as plt
+    
+    signal_start = 2000*units.ns
+    signal_length = 200*units.microsecond
+    daq_window = 1*units.millisecond
 
-  signal_start=2000*ns
-  signal_length=200*microsecond
-  daq_window = 1*millisecond
-  
-  spe = SPE()
-  print spe
+    spe = SPE()
+    print spe
 
-  signal_end = signal_start + signal_length
-  
-  signal_t, signal_PE = spe.SpePulseTrain(signal_start,signal_end,daq_window)
+    signal_end = signal_start + signal_length
 
-  plot_signal(signal_t/ns,signal_PE/muA, 
-                  title = 'Input Signal: PE Train', 
-                  signal_start=0*ns, signal_end=len(signal_t)/ns, 
-                  units='muA')
+    signal_t, signal_PE = spe.SpePulseTrain(signal_start, signal_end,
+                                            daq_window)
 
-  plt.show()
+    putil.plot_signal(signal_t/units.ns, signal_PE/units.muA,
+                      title='Input Signal: PE Train',
+                      signal_start=0*units.ns,
+                      signal_end=len(signal_t)/units.ns,
+                      units='muA')
 
+    plt.show()
