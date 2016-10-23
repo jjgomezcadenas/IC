2702287247fcfba4cec0038f859e35ec78ad68e3
Gonzalo <gonzaponte@gmail.com>
2016-10-23 14:53:56 +0200

    environment variables used

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 export PFMW=/System/Library/Frameworks/Python.framework/Versions/Current/
 export ICDIR=/Users/jjgomezcadenas/Documents/Development/NEXT/IC
+export ICDBDIR=$ICDIR/Database
 export PYTHONPATH=$ICDIR:$ICDIR/Cities:$ICDIR/Cython:$ICDIR/Sierpe:$ICDIR/Core
 export PYTHONPATH=$PYTHONPATH:$ROOTSYS/lib:/usr/local/lib:/$PFMW
 export PS1="IC->"
