58338da0c614fc5b9f36e5e8fa1f6354749f771c
Brais <brais.pp@gmail.com>
2016-11-07 22:15:53 +0100

    Fit errors added

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
-#export PFMW=/System/Library/Frameworks/Python.framework/Versions/Current/
-export ICDIR=/home/brais/Documents/Next/IC/IC
+export PFMW=/System/Library/Frameworks/Python.framework/Versions/Current/
+export ICDIR=/Users/jjgomezcadenas/Documents/Development/NEXT/IC
 export ICDBDIR=$ICDIR/Database
 export ICDATADIR=$ICDIR/data
 export PYTHONPATH=$ICDIR:$ICDIR/Cities:$ICDIR/Cython:$ICDIR/Sierpe:$ICDIR/Core
-#export PYTHONPATH=$PYTHONPATH:/usr/local/lib:/$PFMW
-export NPPATH=~/anaconda2/lib/python2.7/site-packages/numpy/core/include/ 
+export PYTHONPATH=$PYTHONPATH:/usr/local/lib:/$PFMW
 export PS1="IC->"
