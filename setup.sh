ad941b012481c8bd198878b8929330cddaec4682
Gonzalo <gonzaponte@gmail.com>
2016-11-14 16:07:17 +0100

    fixes for cython

--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ export ICDATADIR=$ICDIR/data
 export PYTHONPATH=$ICDIR:$ICDIR/Cities:$ICDIR/Cython:$ICDIR/Sierpe:$ICDIR/Core
 export PYTHONPATH=$PYTHONPATH:/usr/local/lib:/$PFMW
 export PS1="IC->"
+
+###### Uncomment and fill with you own path to numpy
+#export NPPATH=/Applications/anaconda2/lib/python2.7/site-packages/numpy/core/include/
