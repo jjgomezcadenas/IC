dd042df45595791b70c94400d67b53d2d0509f34
jmbenlloch <info@jmbenlloch.net>
2016-11-16 18:25:29 +0100

    Fix setup.sh & Instructions in icompile.py

--- icompile.py
+++ icompile.py
@@ -9,3 +9,4 @@ np_path = '/'.join(np.__file__.split('/')[:-1]) + '/core/include'
 setup(ext_modules=cythonize('ICython/cBLR.pyx'),
       include_dirs=[np_path])
 
+#to run: python icompile.py build_ext --inplace
