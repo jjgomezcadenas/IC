8518b62da4e0fea38332914574af32625556f021
jmbenlloch <info@jmbenlloch.net>
2016-11-17 15:23:27 +0100

    Update db test

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,40 +1,62 @@
 import unittest
 import tables
 from Database import download
-from Database import loadDB
+import Database.loadDB as DB
+import os
 
 
 class dbTest(unittest.TestCase):
+
+    def setUp(self):
+        download.loadDB()
+        self.localdb = os.environ['ICDIR'] + '/Database/localdb.h5'
 
     def test_numberOfPMTs(self):
         """
         Check that we retrieve the correct number of PMTs
         """
-        download.loadDB()
-        h5out = tables.open_file('Database/localdb.h5', 'r')
+        h5out = tables.open_file(self.localdb, 'r')
         nrows = h5out.root.Sensors.DataPMT.nrows
         h5out.close()
         self.assertEqual(nrows, 12)
+
+        pmts = DB.DataPMT()
+        self.assertEqual(pmts.shape[0], 12)
 
     def test_numberOfSiPMs(self):
         """
         Check that we retrieve the correct number of SiPMs
         """
-        download.loadDB()
-        h5out = tables.open_file('Database/localdb.h5', 'r')
+        localdb = os.environ['ICDIR'] + '/Database/localdb.h5'
+        h5out = tables.open_file(self.localdb, 'r')
         nrows = h5out.root.Sensors.DataSiPM.nrows
         h5out.close()
         self.assertEqual(nrows, 1792)
+
+        sipms = DB.DataSiPM()
+        self.assertEqual(sipms.shape[0], 1792)
 
     def test_SiPMNoise(self):
         """
         Check we have noise for all SiPMs and energy of each bin
         """
-        download.loadDB()
-        noise, energy, baseline = loadDB.SiPMNoise()
+        noise, energy, baseline = DB.SiPMNoise()
         self.assertEqual(noise.shape[0], baseline.shape[0])
         self.assertEqual(noise.shape[0], 1792)
         self.assertEqual(noise.shape[1], energy.shape[0])
+
+    def test_DetectorGeometry(self):
+        """
+        Check Detector Geometry
+        """
+        geo = DB.DetectorGeo()
+        self.assertEqual(geo['xmin'][0], -198)
+        self.assertEqual(geo['xmax'][0], 198)
+        self.assertEqual(geo['ymin'][0], -198)
+        self.assertEqual(geo['ymax'][0], 198)
+        self.assertEqual(geo['zmin'][0], 0)
+        self.assertEqual(geo['zmax'][0], 532)
+        self.assertEqual(geo['rmax'][0], 198)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
