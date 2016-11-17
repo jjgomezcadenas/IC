d2878dffdfd4cc3192f0543d1e1af3e5a439f7bb
jjgomezcadenas <jjgomezcadenas@gmail.com>
2016-11-17 17:54:35 +0100

    ISIDORA
    Last version of ISIDORA with a major refactoring. A new version of
    deconvolution using accumulator in CYTHON

--- Cities/ISIDORA.py
+++ Cities/ISIDORA.py
@@ -9,6 +9,9 @@ What ISIDORA does:
 
 Change Log:
 18/10 JJG added cython module. PRE-RELEASE
+
+11.11 JJGC: A major refactoring of the code, now based in a much improved
+deconv algorithm
 """
 
 from __future__ import print_function
@@ -22,55 +25,38 @@ import Core.system_of_units as units
 from Core.LogConfig import logger
 from Core.Configure import configure, define_event_loop
 
-import Sierpe.FEParam as FP
-import Sierpe.FEE2 as FE
-from ICython import cBLR
+import Sierpe.FEE as FE
+import ICython.cBLR as cblr
+import Database.loadDB as DB
 
 
-def accumulator_coefficients(CA, NPMT, len_WF):
-    """
-    Compute the accumulator coefficients for DBLR
-    It computes the inverse function of the HPF and takes
-    the accumulator as the value of the function anywhere
-    but the first bin (the inverse is a step function with
-    constant value equal to the accumulator)
-    CA are the values of the capacitances defining the filter
-    (1/(2CR)) for each PMT
-    """
-    coef_acc = np.zeros(NPMT, dtype=np.double)
-
-    signal_t = np.arange(0.0, len_WF*1., 1., dtype=np.double)
-
-    for j in range(NPMT):
-        fee = FE.FEE(C=CA[j], R=FP.R, f=FP.freq_LPF, RG=FP.V_GAIN)
-        signal_inv_daq = fee.InverseSignalDAQ(signal_t)  # inverse function
-        coef_acc[j] = signal_inv_daq[1]  # any index is valid, function is flat
-
-    return coef_acc
-
-
-def DBLR(pmtrd, event_number, coeff_acc, mau_len=250,
-         thr1=FP.NOISE_ADC, thr2=0, thr3=FP.NOISE_ADC):
+def DBLR(pmtrwf, event_number, n_baseline=500, thr_trigger=5,
+         thr_acum=2000,
+         acum_discharge_length = 5000,
+         acum_tau=2500,
+         acum_compress=0.01):
     """
     Peform Base line Restoration
-    coeff_acc is an array with the coefficients of the accumulator
-    Threshold 1 is used to decide when raw signal raises up above trigger line
-    Threshold 2 is used to decide when reco signal is above trigger line
-    Threshold 3 is used to compare Raw and Rec signal
     """
+    DataPMT = DB.DataPMT()
+    NPMT = pmtrwf.shape[1]
+    CWF = {}
+    ACUM = {}
 
-    NPMT = pmtrd.shape[1]
-    BLRS = []
-
-    for j in range(NPMT):
-        sgn_raw = FP.ceiling - pmtrd[event_number, j]
-        sgn_rec, MAU, pulse_on, wait_over = cBLR.BLR(sgn_raw,
-                                                     coeff_acc[j],
-                                                     mau_len, thr1, thr2, thr3)
-
-        BLRS.append((sgn_rec, MAU, pulse_on, wait_over))
-
-    return zip(*BLRS)
+    for pmt in range(NPMT):
+        signal_r, acum = cblr.\
+          deconvolve_signal_acum(pmtrwf[event,pmt],
+                                 n_baseline=n_baseline,
+                                 coef_clean=DataPMT.coeff_c[pmt],
+                                 coef_blr=DataPMT.coeff_blr[pmt],
+                                 thr_trigger=thr_trigger,
+                                 thr_acum=thr_acum,
+                                 acum_discharge_length=acum_discharge_length,
+                                 acum_tau=acum_tau,
+                                 acum_compress=acum_compress)
+        CWF[pmt] = signal_r
+        ACUM[pmt] = acum
+        return CWF, ACUM
 
 
 def ISIDORA(argv):
@@ -98,25 +84,28 @@ def ISIDORA(argv):
     FIRST_EVT = CFP["FIRST_EVT"]
     LAST_EVT = CFP["LAST_EVT"]
     RUN_ALL = CFP["RUN_ALL"]
-    COEFF = CFP["COEF"]
-    CA = np.array(CFP["CA"]) * units.nF
-    AC = np.array(CFP["AC"])
-    MAU_LEN = CFP["MAU_LEN"]
-    NSIGMA1 = CFP["NSIGMA1"]
-    NSIGMA2 = CFP["NSIGMA2"]
-    NSIGMA3 = CFP["NSIGMA3"]
-
+    N_BASELINE = CFP["N_BASELINE"]
+    THR_TRIGGER = CFP["THR_TRIGGER"]
+    THR_ACUM = CFP["THR_ACUM"]
+    ACUM_DISCHARGE_LENGTH = CFP["ACUM_DISCHARGE_LENGTH"]
+    ACUM_TAU = CFP["ACUM_TAU"]
+    ACUM_COMPRESS = CFP["ACUM_COMPRESS"]
     NEVENTS = LAST_EVT - FIRST_EVT
 
     logger.info("Debug level = {}".format(DEBUG_LEVEL))
     logger.info("Input path ={}; file_in ={} ".format(PATH_IN, FILE_IN))
     logger.info("First event = {} last event = {} "
                 "# events requested = {}".format(FIRST_EVT, LAST_EVT, NEVENTS))
-    logger.info("MAU length = {}, n_sigma1 = {} "
-                "n_sigma2 = {} n_sigma3 = {}".format(MAU_LEN, NSIGMA1,
-                                                     NSIGMA2, NSIGMA3))
-    logger.info("CA  = {} nF".format(CA/units.nF))
-    logger.info("Accumulator Coefficients = {}".format(AC))
+    logger.info("Baseline calculation length = {}"
+                "n_sigma for trigger = {}".format(N_BASELINE, THR_TRIGGER)
+
+    logger.info("""Accumulator Parameters:
+                accumulator threshold = {}
+                length for discharge = {}
+                tau for discharge = {}
+                compression factor = {}
+                """.format(THR_ACUM, ACUM_DISCHARGE_LENGTH,
+                           ACUM_TAU, ACUM_COMPRESS))
 
     # open the input file in mode append
     with tables.open_file("{}/{}".format(PATH_IN, FILE_IN), "a") as h5in:
@@ -138,37 +127,13 @@ def ISIDORA(argv):
                                     atom=tables.Int16Atom(),
                                     shape=(0, NPMT, PMTWL),
                                     expectedrows=NEVENTS_DST)
+        if "/RD/pmtacum" in h5in:
+            h5in.remove_node("/RD", "pmtacum")
 
-        # Create a group to store BLR configuration
-        # Remove already existing nodes in file (they will be overwritten)
-        if "/BLR" not in h5in:
-            h5in.create_group(h5in.root, "BLR")
-        if "/BLR/mau" in h5in:
-            h5in.remove_node("/BLR", "mau")
-        if "/BLR/pulse_on" in h5in:
-            h5in.remove_node("/BLR", "pulse_on")
-        if "/BLR/wait_over" in h5in:
-            h5in.remove_node("/BLR", "wait_over")
-
-        # mau, acummulator, pulse_on and wait_over stored for pmt 0
-        mau = h5in.create_earray(h5in.root.BLR, "mau",
-                                 atom=tables.Int16Atom(),
-                                 shape=(0, PMTWL),
-                                 expectedrows=NEVENTS_DST)
-
-        pulse_on = h5in.create_earray(h5in.root.BLR, "pulse_on",
-                                      atom=tables.Int16Atom(),
-                                      shape=(0, PMTWL),
-                                      expectedrows=NEVENTS_DST)
-
-        wait_over = h5in.create_earray(h5in.root.BLR, "wait_over",
-                                       atom=tables.Int16Atom(),
-                                       shape=(0, PMTWL),
-                                       expectedrows=NEVENTS_DST)
-
-        # compute the accumulator coefficients from the nominal values
-        # of capacitances (if COEF =0) or use previously computed coeff
-        coeff_acc = AC if COEFF else accumulator_coefficients(CA, NPMT, PMTWL)
+        pmtacum = h5in.create_earray(h5in.root.RD, "pmtacum",
+                                    atom=tables.Int16Atom(),
+                                    shape=(0, NPMT, PMTWL),
+                                    expectedrows=NEVENTS_DST)
 
         # LOOP
         first_evt, last_evt, print_mod = define_event_loop(FIRST_EVT, LAST_EVT,
@@ -181,25 +146,23 @@ def ISIDORA(argv):
             if not i % print_mod:
                 logger.info("-->event number = {}".format(i))
 
-            signal_r, xmau, pulse_, wait_ = DBLR(pmtrd_, i, coeff_acc,
-                                                 mau_len=MAU_LEN,
-                                                 thr1=NSIGMA1*FP.NOISE_ADC,
-                                                 thr2=NSIGMA2*FP.NOISE_ADC,
-                                                 thr3=NSIGMA3*FP.NOISE_ADC)
-            # append to pmtcwd
-            pmtcwf.append(np.array(signal_r).reshape(1, NPMT, PMTWL))
+            signal_r, acum = DBLR(pmtrd_, i,
+                             n_baseline=N_BASELINE,
+                             thr_trigger=THR_TRIGGER,
+                             thr_acum=THR_ACUM,
+                             acum_discharge_length=ACUM_DISCHARGE_LENGTH,
+                             acum_tau=ACUM_TAU,
+                             acum_compress=ACUM_COMPRESS)
 
-            # append BLR variables
-            mau.append(xmau[0].reshape(1, PMTWL))
-            pulse_on.append(pulse_[0].reshape(1, PMTWL))
-            wait_over.append(wait_[0].reshape(1, PMTWL))
+            # append to pmtcwf
+            pmtcwf.append(np.array(signal_r).reshape(1, NPMT, PMTWL))
+            # append to pmtacum
+            pmtacum.append(np.array(acum).reshape(1, NPMT, PMTWL))
 
         t1 = time()
         dt = t1 - t0
         pmtcwf.flush()
-        mau.flush()
-        pulse_on.flush()
-        wait_over.flush()
+        pmtacum.flush()
 
         print("ISIDORA has run over {} events in {} seconds".format(i+1, dt))
     print("Leaving ISIDORA. Safe travels!")
